9acf33754213c0eafdd477cc37c64fc71cdf1837
renzolo34 <renzo.lopez@tecsup.edu.pe>
2023-06-18 23:02:44 -0500

    Agregar archivos de proyecto.

---
+++ AppComedor/AppComedor/View/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using AppComedor.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppComedor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var viewModel = new MainViewModel(Navigation);
+            BindingContext = viewModel;
+        }
+    }
+}
---
+++ AppComedor/AppComedor/ViewModel/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿using AppComedor.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace AppComedor.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private INavigation navigation;
+
+        public MainViewModel(INavigation navigation)
+        {
+            this.navigation = navigation;
+        }
+
+        public async Task NavegarWelcome()
+        {
+            await navigation.PushAsync(new Welcome());
+        }
+
+        public ICommand NavegarCommand => new Command(async () => await NavegarWelcome());
+
+        
+    }
+
+
+}
